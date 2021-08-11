76d271ee14fde58842c4d892a9668483006e3511
martier <martier@oocl.com>
2021-08-11 16:31:56 +0800

    [SpringBootPrac][Francis&Eric] feat: add implementation to getEmployeesByPagination

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -10,12 +10,15 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
-    private List<Employee> employees = new ArrayList<>();
+    private final List<Employee> employees = new ArrayList<>();
 
     public EmployeesController() {
         employees.add(new Employee(1, "Francis", 24, "male", 99));
         employees.add(new Employee(2, "Eric", 22, "male", 99));
         employees.add(new Employee(3, "Jewel", 22, "female", 99));
+        employees.add(new Employee(4, "Francis", 24, "male", 99));
+        employees.add(new Employee(5, "Eric", 22, "male", 99));
+        employees.add(new Employee(6, "Jewel", 22, "female", 99));
     }
 
     @GetMapping
@@ -25,15 +28,21 @@ public class EmployeesController {
 
     @GetMapping(path = "/{id}")
     public Employee getEmployee(@PathVariable Integer id) {
-        return  employees.stream()
+        return employees.stream()
                 .filter(employee -> employee.getId().equals(id))
                 .findFirst()
                 .orElse(null);
     }
 
-
     @RequestMapping(params = "gender")
-    public List<Employee> getGender(@RequestParam String gender){
+    public List<Employee> getGender(@RequestParam String gender) {
         return employees.stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
+    }
+
+    @GetMapping(params = {"pageIndex", "pageSize"})
+    public List<Employee> getEmployeesByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
+        return employees.stream()
+                .skip((long) (pageIndex - 1) * pageSize)
+                .limit(pageSize).collect(Collectors.toList());
     }
 }
