73c038d65e47f146a5055f19499f8ce2d3dd1854
David Salonga <fdc.salonga@gmail.com>
2021-08-11 16:20:16 +0800

    [SpringBootPrac][Francis & Eric] feat: implemented employeeId and get gender

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,12 +1,11 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -16,10 +15,25 @@ public class EmployeesController {
     public EmployeesController() {
         employees.add(new Employee(1, "Francis", 24, "male", 99));
         employees.add(new Employee(2, "Eric", 22, "male", 99));
+        employees.add(new Employee(3, "Jewel", 22, "female", 99));
     }
 
     @GetMapping
     public List<Employee> getEmployees() {
         return employees;
+    }
+
+    @GetMapping(path = "/{id}")
+    public Employee getEmployee(@PathVariable Integer id) {
+        return  employees.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    @RequestMapping(params = "gender")
+    public List<Employee> getGender(@RequestParam String gender){
+        return employees.stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
     }
 }
