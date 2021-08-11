f3daebfe6d5dde9a960da99440764e6c965b4018
martier <martier@oocl.com>
2021-08-11 21:01:02 +0800

    [SpringBoot][Eric] fix: change employees to getEmployees

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -43,7 +43,7 @@ class EmployeesController {
 
     @GetMapping(params = {"pageIndex", "pageSize"})
     public List<Employee> getEmployeesByPagination(@RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
-        return employees.stream()
+        return getEmployees().stream()
                 .skip((long) (pageIndex - 1) * pageSize)
                 .limit(pageSize).collect(Collectors.toList());
     }
