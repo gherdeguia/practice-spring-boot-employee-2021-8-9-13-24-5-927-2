feda0539dbb9b8a0e5c970b7f4879cc7350fb9c9
martier <martier@oocl.com>
2021-08-12 02:32:17 +0800

    [SpringBootCompany][Eric] feat: created CompaniesController then added getCompanies

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompaniesController.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompaniesController {
+
+    @Autowired
+    private final CompanyService companyService;
+
+    public CompaniesController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public List<Company> getCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+}
